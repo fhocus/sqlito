9776e300528fc96d70e1226cddc27ebf5d77c431
fhocus <fhocus.me@gmail.com>
2024-11-25 11:16:07 -0500

    setup project

---
+++ include/SQLito.hpp
@@ -0,0 +1,6 @@
+#ifndef SQLITO_HPP
+#define SQLITO_HPP
+
+#define SQLITO_DEBUG_MODE 1
+
+#endif // SQLITO_HPP
---
+++ test/SQLitoTest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SQLito.hpp"
+
+int main()
+{
+    std::cout << "Hola" << std::endl;
+    return 0;
+}
